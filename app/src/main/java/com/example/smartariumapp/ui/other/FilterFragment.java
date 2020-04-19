5c5f45af1d2aff4b71f47f5a6102f67dc1458329
Dezyderata <agnieszkarydzyk@gmail.com>
2020-04-19 15:47:33 +0200

    Filter fragment

--- app/src/main/java/com/example/smartariumapp/ui/other/FilterFragment.java
+++ app/src/main/java/com/example/smartariumapp/ui/other/FilterFragment.java
@@ -1,65 +1,133 @@
 package com.example.smartariumapp.ui.other;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smartariumapp.R;
+import com.example.smartariumapp.data.DataHolder;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FilterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FilterFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int identifier = 0;
+    private Button[] myButtons;
+    private String[] myArray;
+    private int[] myColors;
+    private Button bt_back;
 
     public FilterFragment() {
         // Required empty public constructor
     }
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FilterFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FilterFragment newInstance(String param1, String param2) {
-        FilterFragment fragment = new FilterFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+        final View root = inflater.inflate(R.layout.fragment_filter, container, false);
+        final TextView textView = root.findViewById(R.id.text_filter);
+        final String title = getResources().getStringArray(R.array.other_main_strings)[identifier];
+        textView.setText(title);
+        final LinearLayout layout = root.findViewById(R.id.linearLayout);
+        myArray = getResources().getStringArray(R.array.filter_strings);
+        myColors = getResources().getIntArray(R.array.water_parameters_colors);
+        int n = myArray.length;
+        myButtons = new Button[n];
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        for(int i = 0; i < 2; i++){
+            myButtons[i] = setMyButton(i, layout.getContext(), root, title);
+            final int my_val = i;
+            final String main_button_string = myArray[i];
+            myButtons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    myButtons[1 - my_val].setVisibility(View.INVISIBLE);
+                   if (myButtons[2] == null) {
+                       for(int k = 2; k < myArray.length; k++){
+                          myButtons[k] = setMyButton(k, layout.getContext(), root, title);
+                          final String ans = myArray[k];
+                          myButtons[k].setOnClickListener(new View.OnClickListener() {
+                              @Override
+                              public void onClick(View v) {
+                                  check_set_parameters(ans, root, main_button_string);
+                              }
+                          });
+                           layout.addView(myButtons[k]);
+                       }
+                   }else{
+                       myButtons[1 - my_val].setVisibility(View.VISIBLE);
+                       for(int k = 2; k < myArray.length; k++){
+                           layout.removeView(myButtons[k]);
+                           myButtons[k] = null;
+
+                       }
+                    }
+                }
+
+            });
+            layout.addView(myButtons[i]);
         }
+        bt_back = setMyButton(myArray.length, layout.getContext(),root, title);
+        bt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(root).navigate(R.id.nav_other);
+            }
+        });
+        layout.addView(bt_back);
+        return root;
+    }
+    private void check_set_parameters(String ans, View root, String title){
+        if(DataHolder.isKeyIn(title)){
+            DataHolder.removeByKey(title);
+            Toast.makeText(getActivity(), "Usunięto " + title + " "+ ans +" z danych do wysłania!", Toast.LENGTH_SHORT).show();
+
+        }else{
+            DataHolder.setMyData(title, ans);
+            Toast.makeText(getActivity(), "Dodano "+ title + " "+ ans + " do danych do wyałania!", Toast.LENGTH_SHORT).show();
+        }
+        try {
+            finalize();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        Navigation.findNavController(root).navigate(R.id.nav_other);
+
+    }
+    private Button setMyButton(int i, Context context, final View root, final  String title){
+        Button button = new Button(context);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.setMargins(0, 10, 0, 10);
+        button.setLayoutParams(params);
+        final String ans;
+        if(i == this.myArray.length){
+            ans = getResources().getString(R.string.back);
+            button.setText(ans);
+            button.setBackgroundColor(getResources().getColor(R.color.back));
+
+        }else {
+            ans = this.myArray[i];
+            button.setText(ans);
+            button.setBackgroundColor(this.myColors[i]);
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                check_set_parameters(ans, root, title);
+            }
+        });
+        return button;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_filter, container, false);
-    }
 }
