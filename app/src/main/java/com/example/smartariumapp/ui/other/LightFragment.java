e6c66fab111bdcb05e6e0e6e4f40eb6f0fbf5ec8
Dezyderata <agnieszkarydzyk@gmail.com>
2020-04-20 15:13:14 +0200

    Maintenance and light gragment

--- app/src/main/java/com/example/smartariumapp/ui/other/LightFragment.java
+++ app/src/main/java/com/example/smartariumapp/ui/other/LightFragment.java
@@ -2,64 +2,89 @@ package com.example.smartariumapp.ui.other;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smartariumapp.R;
+import com.example.smartariumapp.data.DataHolder;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LightFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class LightFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private int identifier = 1;
+    private Button bt_back, bt_send;
+    private String[] myStrings, lightAnsValues;
+    private EditText[] myEditText;
     public LightFragment() {
         // Required empty public constructor
     }
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LightFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LightFragment newInstance(String param1, String param2) {
-        LightFragment fragment = new LightFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        final View root = inflater.inflate(R.layout.fragment_light, container, false);
+        final TextView textView = root.findViewById(R.id.text_light);
+        final String title = getResources().getStringArray(R.array.other_main_strings)[identifier];
+        textView.setText(title);
+        lightAnsValues = getResources().getStringArray(R.array.light_ans_values);
+        bt_back = root.findViewById(R.id.bt_back);
+        bt_send = root.findViewById(R.id.bt_send);
+        myEditText = new EditText[4];
+        myEditText[0] = root.findViewById(R.id.light_on_from);
+        myEditText[1] = root.findViewById(R.id.light_on_to);
+        myEditText[2] = root.findViewById(R.id.light_conditional_from);
+        myEditText[3] = root.findViewById(R.id.light_conditional_to);
+        myStrings = new String[4];
+
+
+        bt_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean check_status = true;
+                for(int k = 0; k < myEditText.length; k++){
+                    myStrings[k] = myEditText[k].getText().toString();
+                }
+                for(int k = 0; k < myEditText.length; k += 2){
+                    if(!myStrings[k].isEmpty() && !myStrings[k+1].isEmpty()){
+                        check_status = false;
+                        check_set_parameters(lightAnsValues[k], myStrings[k], root);
+                        check_set_parameters(lightAnsValues[k + 1], myStrings[k + 1], root);
+                    }
+                }
+                if(check_status){
+                    Toast.makeText(getActivity(), "Brak wystaraczającej ilości danych", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        bt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(root).navigate(R.id.nav_other);
+            }
+        });
+        return root;
     }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    private void check_set_parameters(String parameter, String ans, View root){
+        if(DataHolder.isKeyIn(parameter)){
+            Toast.makeText(getActivity(), "Najpierw należy wysłać już zgromadzone dane!", Toast.LENGTH_SHORT).show();
+            Navigation.findNavController(root).navigate(R.id.nav_other);
+            try {
+                finalize();
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        }else{
+            DataHolder.setMyData(parameter, ans);
+            Toast.makeText(getActivity(), parameter+ " "+ans, Toast.LENGTH_SHORT).show();
+            Navigation.findNavController(root).navigate(R.id.nav_other);
         }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_light, container, false);
     }
 }
