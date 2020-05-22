7e6fb795d1c0c654419aeb35b61899f9c893374c
Robert Ch <s15753@pjwstk.edu.pl>
2020-05-22 17:47:50 +0200

    login changes

--- app/src/main/java/com/example/smartariumapp/data/model/RestUtils.java
+++ app/src/main/java/com/example/smartariumapp/data/model/RestUtils.java
@@ -1,5 +1,10 @@
 package com.example.smartariumapp.data.model;
 
+import android.content.res.Resources;
+import android.util.Log;
+
+import com.example.smartariumapp.R;
+
 /**
  *  Class take Retrofit client and return calls from ZabbixApiService.
  *
@@ -14,7 +19,6 @@ public class RestUtils {
      * Base URL for retrofit client.
      */
     public static final String BASE_URL = "https://zolwik.tk";
-//            Resources.getSystem().getString(R.string.base_url);
 
     /**
      * This public static method will return calls for all method in ZabbixApiService.
--- app/src/main/java/com/example/smartariumapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/smartariumapp/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.smartariumapp.ui.home;
 
 import android.os.Bundle;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
